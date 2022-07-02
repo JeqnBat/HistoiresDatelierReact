e8cd505c2db70b9c356acab7f5a64843f9a63e6b
Marco <46994366+JeqnBat@users.noreply.github.com>
2022-07-02 14:45:25 +0200

    item focus solution 100% for desktop

--- src/components/FocusItem.jsx
+++ src/components/FocusItem.jsx
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom'
 import './css/focus-item.css'
 
-const FocusItem = ({ visible, item }) => {
+const FocusItem = ({ visible, item, events }) => {
   if (visible) {
     const style = {
       backgroundImage: `url(${item.imageUrl[0]})`,
@@ -8,15 +9,29 @@ const FocusItem = ({ visible, item }) => {
       backgroundPosition: 'center'
     }
     return (
-      <div id='screener'>
-        <div><i className='fa-solid fa-chevron-left'></i></div>
-        <div style={style}>
-          <div><i className='fa-solid fa-xmark'></i></div>
-          <div>
-            <span>fiche détaillée</span>
+      <div id='screener' onClick={events}>
+        <div id='previous' onClick={events}>
+          <i className='fa-solid fa-chevron-left'></i></div>
+        <div onClick={events}>
+          <div style={style}></div>
+          <div id='close' onClick={events}>
+            <i className='fa-solid fa-xmark'></i>
+            </div>
+          <div className='spacer'></div>
+          <div className='spacer'></div>
+          <div className='spacer'></div>
+          <div className='spacer'></div>
+          <div id='details' onClick={events}>
+            <Link to={`${item.category}/article=${item.id}`}>
+              <span>
+                fiche détaillée
+              </span>
+            </Link>
           </div>
         </div>
-        <div><i className="fa-solid fa-chevron-right"></i></div>
+        <div id='next' onClick={events}>
+          <i className="fa-solid fa-chevron-right"></i>
+        </div>
       </div>
     )
   } else {
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -5,20 +5,52 @@ import './css/grid.css'
 import './css/focus-item.css'
 
 const Grid = ({ products }) => {
-  const [focused, setFocused] = useState(false)
-  const [focusedID, setFocusedID] = useState(null)
+  /* CHILD COMPONENT 'FOCUSITEM' LOGIC ___________________ */
+  const [popUp, setPopup] = useState(false)
+  const [item, setItem] = useState(null)
+
+  const handleClick = (e) => {
+    setPopup(true)
+    setItem(products.find(el => el.id === e.currentTarget.id))
+  }
+  const events = (e) => {
+  // Prevent bubbling so 'screener click' does not remove other events
+    e.stopPropagation()
+    switch (e.currentTarget.id) {
+      case 'previous':
+        let prevItemID = item.id > 0 ? (Number(item.id) - 1).toString() : item.id
+        setItem(products.find(el => el.id === prevItemID))
+        break
+      case 'next':
+        let nextItemID = item.id < products.length - 1 ? (Number(item.id) + 1).toString() : item.id
+        setItem(products.find(el => el.id === nextItemID))
+        break
+      case 'close':
+        setPopup(false)
+        break
+      case 'details':
+        setPopup(false)
+        break
+      case '' :
+        break
+      default:
+        setPopup(false)
+        break
+    }
+  }
+
+  /* COMPONENTS LOGIC ____________________________________ */
   let a = useLocation()
   let url = a.pathname.match(/[^\/]*$/)[0]
   let showArticle = Boolean(url.includes('article'))
 
-  const handleClick = (e) => {
-    setFocused(true)
-    setFocusedID(e.currentTarget.id)
-  }
-
   return (
     <>
-      <FocusItem visible={focused} item={products.filter(el => el.id === focusedID)[0]} />
+      <FocusItem 
+        visible={popUp}
+        item={item}
+        events={events}
+      />
       <div id='grid' className={showArticle ? 'hide' : ''}>
         {products.map((product) => (
           <div
