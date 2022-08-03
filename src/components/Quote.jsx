05412dea603a22c96bf8c318881aad8f14d50ff1
Marco <46994366+JeqnBat@users.noreply.github.com>
2022-08-03 23:01:20 +0200

    full responsive full solution step 1

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -4,16 +4,20 @@ import './css/quote.css'
 const Quote = ({ data }) => {
   return (
     <>
-    <section id="quote">
-      <span>“</span>
-      <h2>{data.homePage.quote}</h2>
-      <span>”</span>
-    </section>
-    <div className='spacinho'></div>
-    <section id="descr">
-      <h1>{data.homePage.latelier}</h1>
-      <div>{paragraphs(data.homePage.latelierdescr)}</div>
-    </section>
+      <section id="quote">
+        <span>“</span>
+        <h2>{data.homePage.quote}</h2>
+        <span>”</span>
+      </section>
+      <div className='spacinho'></div>
+      <section id="descr">
+        <div></div>
+        <div>
+          <h1>{data.homePage.latelier}</h1>
+          <div>{paragraphs(data.homePage.latelierdescr)}</div>
+        </div>
+        <div></div>
+      </section>
     </>
   )
 }
