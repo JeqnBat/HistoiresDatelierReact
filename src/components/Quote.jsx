ee782a41b658020810690c215a9718e8db2cd5e8
Marco <46994366+JeqnBat@users.noreply.github.com>
2022-08-04 12:38:57 +0200

    main layout work #CSS

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -4,19 +4,23 @@ import './css/quote.css'
 const Quote = ({ data }) => {
   return (
     <>
-      <section id="quote">
-        <span>“</span>
-        <h2>{data.homePage.quote}</h2>
-        <span>”</span>
+      <section id='quote'>
+        <div className='sider'></div>
+        <div>
+          <span>“</span>
+          <h2>{data.homePage.quote}</h2>
+          <span>”</span>
+        </div>
+        <div className='sider'></div>
       </section>
       <div className='spacinho'></div>
-      <section id="descr">
-        <div></div>
+      <section id='descr'>
+        <div className='sider'></div>
         <div>
           <h1>{data.homePage.latelier}</h1>
           <div>{paragraphs(data.homePage.latelierdescr)}</div>
         </div>
-        <div></div>
+        <div className='sider'></div>
       </section>
     </>
   )
